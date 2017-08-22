e41db4afb07530fbbb441b1148be6b72542b272b
PierreDuc <poulkruijt@gmail.com>
2017-08-22 09:31:36 +0300

    Bug shows when you reference the src folder in the paths object

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from '@app/app.component';
+import { AppComponent } from '@root/app/app.component';
 
 
 @NgModule({
